01c4d9512aa2ea4ac0d0bdfe855e8d52ce9bbcbe
Freeman-md <freemanmadudili@gmail.com>
2021-05-04 10:53:27 +0100

    Vue Front End For Coursework 2

---
+++ public/lessons.js
@@ -0,0 +1,74 @@
+{
+    "data": [
+        {
+            "Subject": "Mathematics",
+            "Location": "London",
+            "Price": 200,
+            "Spaces": 5,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "English Language",
+            "Location": "Texas",
+            "Price": 300,
+            "Spaces": 7,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Computer Science",
+            "Location": "Texas",
+            "Price": 250,
+            "Spaces": 6,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Physics",
+            "Location": "London",
+            "Price": 150,
+            "Spaces": 5,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Biology",
+            "Location": "New York",
+            "Price": 300,
+            "Spaces": 8,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Economics",
+            "Location": "Florida",
+            "Price": 350,
+            "Spaces": 5,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Accounting",
+            "Location": "Maryland",
+            "Price": 100,
+            "Spaces": 9,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Marketing",
+            "Location": "New York",
+            "Price": 100,
+            "Spaces": 5,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "Geography",
+            "Location": "Maryland",
+            "Price": 150,
+            "Spaces": 6,
+            "Image": "./assets"
+        },
+        {
+            "Subject": "General Studies",
+            "Location": "Florida",
+            "Price": 200,
+            "Spaces": 6,
+            "Image": "./assets"
+        }
+    ]
+}
